77b3aa094b8e832920da66aafdfde190be34c91f
HoJJang <hojjang@gmail.com>
2024-02-26 19:12:53 +0900

    telegram bot example

--- Samples/telegram_bot.py
+++ Samples/telegram_bot.py
@@ -1,9 +1,14 @@
 import telepot 
 import pprint
 
-token = "5435932808:AAH2g1SV793wxWmVEzyFTShCpCxJrYV6GtE"
+token = "5435932808:AAEX4vK-CE_fxZMO74uZ1mu3oQI4mnFpc_Q"
 bot = telepot.Bot(token)
 
 info = bot.getMe()
 pprint.pprint(info)
+
+resp = bot.getUpdates()
+pprint.pprint(resp)
+
+bot.sendMessage(chat_id=1697475589, text="Hi I am a Bot")
 
